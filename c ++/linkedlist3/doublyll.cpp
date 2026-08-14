204a02d9548f703f76174328b0d85c1ec717e967
Vishal Gupta <vguota30122@gmail.com>
2026-08-14 17:31:14 +0530

    ram

---
+++ c ++/linkedlist3/doublyll.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+class Vechile{ //parent class 
+    public:
+    int topspeed;
+    float mileage;
+    string fuel;
+    // private:
+    // int bootspace;
+};
